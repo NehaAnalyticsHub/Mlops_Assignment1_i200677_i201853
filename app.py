2687f9f35466a584d2bf7ecfdb6f898f79b4918a
i200677_Neha <neha.shabbir11@gmail.com>
2024-03-30 23:08:28 -0700

    Fix Flake8 errors

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import joblib
 
 app = Flask(__name__)
